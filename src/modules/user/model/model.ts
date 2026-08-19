301a6dd2d74de1e88e4f9d73c5df9b8620bc6e09
HieuDao1811 <daohuuhieu18112004@gmail.com>
2026-08-19 11:37:53 +0700

    feat(user): add a little config

---
+++ src/modules/user/model/model.ts
@@ -0,0 +1,34 @@
+import z from "zod";
+
+export enum Gender {
+  MALE = 'male',
+  FEMALE = 'female',
+  UNKNOWN = 'unknown'
+}
+
+export enum UserRole {
+  ADMIN = 'admin',
+  CUSTOMER = 'customer'
+}
+
+export enum UserStatus {
+  ACTIVE = 'active',
+  INACTIVE = 'inactive',
+  DELETED = 'deleted'
+}
+
+export const UserSchema = z.object({
+  id: z.string(),
+  firstName: z.string(),
+  lastName: z.string(),
+  avatar: z.string().url().optional(),
+  gender: z.enum(Gender).default(Gender.UNKNOWN),
+  email: z.string().email(),
+  password: z.string(),
+  role: z.enum(UserRole).default(UserRole.CUSTOMER),
+  status: z.enum(UserStatus).default(UserStatus.ACTIVE),
+  createdAt: z.date(),
+  updatedAt: z.date()
+});
+
+export type User = z.infer<typeof UserSchema>;

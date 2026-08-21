be6b84f711322aa57670c223340b1a09f7cc5ef2
HieuDao1811 <daohuuhieu18112004@gmail.com>
2026-08-21 21:44:32 +0700

    feat(auth): implement user login

--- src/shared/component/jwt.ts
+++ src/shared/component/jwt.ts
@@ -5,9 +5,9 @@ import { config } from "../component/config";
 
 class JwtTokenService implements ITokenProvider {
   private readonly secretKey: string;
-  private readonly expiresIn: StringValue | number;
-
-  constructor(secretKey: string, expiresIn: StringValue | number) {
+  private readonly expiresIn: StringValue;
+  
+  constructor(secretKey: string, expiresIn: StringValue) {
     this.secretKey = secretKey;
     this.expiresIn = expiresIn;
   }
@@ -15,11 +15,10 @@ class JwtTokenService implements ITokenProvider {
   async generateToken(payload: TokenPayload): Promise<string> {
     return jwt.sign(payload, this.secretKey, { expiresIn: this.expiresIn });
   }
-
   async verifyToken(token: string): Promise<TokenPayload | null> {
     const decoded = jwt.verify(token, this.secretKey) as TokenPayload;
     return decoded;
   }
+  
 }
-
 export const jwtProvider = new JwtTokenService(config.accessToken.secretKey, config.accessToken.expiresIn as StringValue);
